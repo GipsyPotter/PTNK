d04b3a4a63db0e2cb3be4135c3fd530f699c0ca2
ThePotter <nguyentadac@gmail.com>
2022-11-14 19:25:33 +0700

    update

---
+++ bt vong lap/8a.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    long temp, n;
+    int count = 0;
+    cin>>n;
+    while(n <= 0);
+    temp = n;
+    if(n == 0) count = 1;
+    while(temp != 0)
+    {
+        count++;
+        temp = temp / 10;
+    }
+    cout<<count;
+}
