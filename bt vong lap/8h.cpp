e9fe001b314010cfb869f4425803defd88478e3a
ThePotter <nguyentadac@gmail.com>
2022-11-16 11:06:48 +0700

    update

---
+++ bt vong lap/8h.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string n;
+    int index;
+    cin >> n;
+    for (int i = 0; i < n.length(); i++) {
+        if (n[i] != '0') {
+            index = i;
+        }
+
+    }
+    index = n.length() - index - 1;
+    cout << index;
+}
