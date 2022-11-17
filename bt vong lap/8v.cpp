5bca85979a734b64b6832529ee948c7b0b7071b0
Yui301020 <dqdung3112@gmail.com>
2022-11-17 13:13:13 +0700

    modified:   bt vong lap/8v.cpp

--- bt vong lap/8v.cpp
+++ bt vong lap/8v.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
+#include <string>
 #include <iomanip>
 using namespace std;
+
+int main(){
+    string str;
+    cin>>str;
+    int c=0;
+    bool flag=1;
+    for(int i=0;i<str.length();i++){
+        if(flag&&str[i]!='0'){
+            c++;
+            flag=0;
+        }else if(!flag&&str[i]=='0') flag=1;
+    }
+    cout<<c;
+}
