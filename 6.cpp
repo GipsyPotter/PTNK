10ea867c03aae7288603f5be5634d94e280d7277
Yui301020 <dqdung3112@gmail.com>
2022-11-17 12:49:38 +0700

    new file:   6.cpp

---
+++ 6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    if(n==1){
+        cout<<1;
+        return;
+    }else if(n==2){
+        cout<<2;
+        return;
+    }else{
+        int f1=1, f2=2, f3=0, i=3;
+        while(i<=n){
+            f3 = 3*f2-2*f1;
+            f1=f2;
+            f2=f3;
+            i++;
+        }
+        cout<<f3;
+    }
+}
