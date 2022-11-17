cdf9cb0d7dfbc7662bb2390a826db53524ccdbe0
Yui301020 <dqdung3112@gmail.com>
2022-11-17 22:20:11 +0700

    new file:   bt mang 1 chieu/1.cpp 	new file:   bt mang 1 chieu/2.cpp 	new file:   bt mang 1 chieu/3.cpp 	new file:   bt mang 1 chieu/4.cpp 	new file:   bt mang 1 chieu/5.cpp

---
+++ bt mang 1 chieu/5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int c=0;
+    for(int i=0;i<n;i++){
+        int a;
+        cin>>a;
+        if(a%2!=0) c++;
+    }
+    cout<<c;
+}
