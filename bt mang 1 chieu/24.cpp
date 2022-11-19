2810eed09874944fa593d136a52dbea37b74d65c
ThePotter <nguyentadac@gmail.com>
2022-11-19 10:02:46 +0700

    update Sat4

---
+++ bt mang 1 chieu/24.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int n, x;
+    cin >> n;
+    int a[n];
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    cin >> x;
+    for (int i = 0; i < n; i++){
+        if (a[i] == x){
+            cout << i << " " << endl;
+            return 0;
+        }
+    }
+
+}
