03f1dc84c7efe63fe21fa406d7a126b5ea54665f
ThePotter <nguyentadac@gmail.com>
2022-11-19 11:40:20 +0700

    update Sat update

--- bt mang 1 chieu/23.cpp
+++ bt mang 1 chieu/23.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 int main(){
     int n, x;
+    bool detect = false;
     cin >> n;
     int a[n];
     for(int i = 0; i < n; i++){
@@ -11,8 +12,9 @@ int main(){
     cin >> x;
     for (int i = 0; i < n; i++){
         if (a[i] == x){
+            detect = true;
             cout << i << " ";
         }
     }
-
+    if (!detect) cout << -1;
 }
--- bt mang 1 chieu/24.cpp
+++ bt mang 1 chieu/24.cpp
@@ -15,5 +15,6 @@ int main(){
             return 0;
         }
     }
+    cout << -1;
 
 }
