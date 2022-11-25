907618f2c48c58951d64bb609ad0acc74fe32a8a
ThePotter <nguyentadac@gmail.com>
2022-11-25 11:23:24 +0700

    updated

--- bt vong lap/1h.cpp
+++ bt vong lap/1h.cpp
@@ -1,3 +1,19 @@
-//
-// Created by Admin on 11/13/2022.
-//
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+double Solve(int n){
+    double s=1, a=0, b=0;
+    int i =2, m = 2;
+    while(i<=n){
+        if (i%2==0) s = s*(1+1.0/sqrt(i));
+        else s = s*(1-1.0/sqrt(i));
+        i++;
+    }
+    return s;
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<setprecision(2)<<fixed<<Solve(n);
+}
