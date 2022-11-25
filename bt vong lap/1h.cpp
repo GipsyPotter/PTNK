0f8566c943fab8ca2171d74db2cdc8a9a421c4a3
Yui301020 <dqdung3112@gmail.com>
2022-11-25 11:23:39 +0700

    modified:   bt vong lap/1h.cpp

--- bt vong lap/1h.cpp
+++ bt vong lap/1h.cpp
@@ -7,11 +7,11 @@ int main()
 {
     int n;
     cin>>n;
-    double s=0;
+    double s=1;
     for(int i=2; i<=n; i++)
     {
-        if(i%2==0) s+=1+(1/sqrt(i));
-        else s+=1-(1/sqrt(i));
+        if(i%2==0) s*=1+(1.0/sqrt(i));
+        else s*=1-(1.0/sqrt(i));
     }
     cout<<setprecision(2)<<fixed<<s;
     return 0;
