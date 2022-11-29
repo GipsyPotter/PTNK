9ac12d79ec088190a2c2be0dac67d9e64b18dd53
Yui301020 <dqdung3112@gmail.com>
2022-11-30 00:27:09 +0700

    new file:   Other-Club/1(y).cpp

---
+++ Other-Club/1(y).cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string   input   = "2 days 36 hour150minutes";
+
+    uint64_t sec     = 0;
+    uint64_t temp    = 0;
+
+    bool     bSyntax = 0;
+
+    for(int a : input)
+    {
+        a = tolower(a);
+
+        if('0' <= a && a <= '9')
+        {
+            temp = temp * 10 + (a - '0');
+            bSyntax = 1;
+        }
+        else if(a == 'd' && bSyntax)
+        {
+            sec += temp * 86400;
+            temp = 0;
+            bSyntax = 0;
+        }
+        else if(a == 'h' && bSyntax)
+        {
+            sec += temp * 3600;
+            temp = 0;
+            bSyntax = 0;
+        }
+        else if(a == 'm' && bSyntax)
+        {
+            sec += temp * 60;
+            temp = 0;
+            bSyntax = 0;
+        }
+        else if(a == 's' && bSyntax)
+        {
+            sec += temp;
+            temp = 0;
+            bSyntax = 0;
+        }
+    }
+    cout<<sec;
+    return 0;
+}
