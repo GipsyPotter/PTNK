2b3a401df76f8cdd8214e0491cf1196aa2fdbaee
ThePotter <nguyentadac@gmail.com>
2022-12-03 20:28:42 +0700

    update

--- Other-Club/PTNKOJ/power.cpp
+++ Other-Club/PTNKOJ/power.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <cmath>
+
 using namespace std;
 
 int main(){
-    uint64_t a;
-    uint64_t n;
-    uint64_t m;
-    cin>>a>>n>>m;
+    uint64_t a = pow(10,9);
+    uint64_t n = pow(10,9);
+    uint64_t m = 3;
+
     uint64_t res = 1;
     uint64_t pow1;
     pow1 = pow(a, n);
-    res = pow1 % m;
+    res = mod(pow1, m);
     cout<<res;
 }
