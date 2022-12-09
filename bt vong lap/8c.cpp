88b90a95ff68213e1f56efb8932f8a82d38c3574
Yui301020 <dqdung3112@gmail.com>
2022-12-09 11:07:48 +0700

    modified:   bt vong lap/8c.cpp

--- bt vong lap/8c.cpp
+++ bt vong lap/8c.cpp
@@ -2,28 +2,15 @@
 using namespace std;
 int main(){
     long temp, n;
-    int count = 0;
+    int sum = 0;
     cin>>n;
     while(n <= 0);
     temp = n;
-    if(n == 0) count = 1;
     while(temp != 0)
     {
-        count++;
+        int tmp = temp % 10;
+        if(tmp%2==0) sum += tmp;
         temp = temp / 10;
     }
-
-    int array[count];
-    int number = n;
-    for (int i = 5; i >= 0; i--) {
-        array[i] = number % 10;
-        number /= 10;
-    }
-    int count2 = 0;
-    for (int i = 0; i < count; i++) {
-        if (array[i] % 2 == 0) {
-            count2++;
-        }
-    }
-    cout<<count2<<endl;
+    cout<<sum;
 }
