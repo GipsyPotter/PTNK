b5b7451e7cc869cde1d8f4cc626617dfd95a469d
Yui301020 <dqdung3112@gmail.com>
2022-12-09 14:24:53 +0700

    modified:   bt vong lap/8f.cpp

--- bt vong lap/8f.cpp
+++ bt vong lap/8f.cpp
@@ -1,29 +1,17 @@
 #include <iostream>
 using namespace std;
 int main(){
-    long temp, n;
-    int count = 0;
+    uint64_t temp, n, min = 10;
     cin>>n;
-    while(n <= 0);
     temp = n;
-    if(n == 0) count = 1;
     while(temp != 0)
     {
-        count++;
+        int tmp = temp % 10;
+        if(tmp != 0)
+        {
+            if(tmp < min) min = tmp;
+        }
         temp = temp / 10;
     }
-
-    int array[count];
-    int number = n;
-    for (int i = 5; i >= 0; i--) {
-        array[i] = number % 10;
-        number /= 10;
-    }
-    int min = 0;
-    for (int i = 0; i < count; i++) {
-        if (array[i] != 0 && array[i] < min) {
-            min = array[i];
-        }
-    }
-    cout<<min<<endl;
+    cout << min;
 }
