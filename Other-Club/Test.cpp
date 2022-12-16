86ace958795a7e04db692fa95435e970409c2cc3
ThePotter <nguyentadac@gmail.com>
2022-12-16 12:45:48 +0700

    v5 .0

--- Other-Club/Test.cpp
+++ Other-Club/Test.cpp
@@ -3,11 +3,13 @@
 #include <cmath>
 using namespace std;
 int main(){
-    int a, b, c, p;
-    float s;
-    cin >> a >> b >> c;
-    cout << setprecision(2) << fixed;
-    p = (a + b + c)/2;
-    s = sqrt(p*(p-a)*(p-b)*(p-c));
-    cout << s;
+    int n, count = 0;
+    cin >> n;
+    while (n != 1){
+        if (n % 2 == 0) n /= 2;
+        else n = 3 * n + 1;
+        count += 1;
+    }
+    cout << count + 1 ;
+
 }
